dc2093f542ce77fb159719c7fcc53d2794596890
lukehedger <luke.hedger@gmail.com>
2019-05-21 09:19:27 +0100

    Add redis

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/lukehedger/heroku-test
+
+require (
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+)
--- server.go
+++ server.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := redis.NewClient(opt)
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("key", val)
 
 	http.HandleFunc("/", handler)
 
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
